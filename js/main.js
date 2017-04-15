15003825e7f1998f4d311d3e22d51e9d4c059e27
Ahmad Shalla <shallal.net@gmail.com>
2017-04-15 09:39:52 +0200

    no message

--- js/main.js
+++ js/main.js
@@ -1,11 +1,8 @@
  $(document).ready(function(){
-    $('.menu').click(function(e){
-        e.stopPropagation();
-        $('#main_nav').toggleClass('active');
-    });
-
-     $(document).click(function(){
-       
-        $('#main_nav').removeClass('active');
-    });
+   $('#main_nav').click(function(){
+       this.collapse('toggle');
+   });
+   $(document).click(function(){
+       $('.collapse.in').removeClass('in');
+   });
 });
